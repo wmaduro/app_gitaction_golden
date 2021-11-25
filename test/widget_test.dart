24ca97ebe3d55ba227791141a08ac5fca94dd18a
maduro-teste <wmaduro@gmail.com>
2021-11-25 17:43:15 +1300

    golden tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,26 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:app_gitaction_golden/main.dart';
+import 'package:golden_toolkit/golden_toolkit.dart';
+
+import 'test_app.dart';
+
+final _devices = [
+  Device.phone.copyWith(name: 'phone_portrait'),
+  Device(name: 'phone_landscape', size: Device.phone.size.flipped),
+  Device.tabletLandscape,
+];
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  var name = 'teste-app';
+  testGoldens('teste-goldem-maduro', (WidgetTester tester) async {
+    final builder = DeviceBuilder()
+      ..overrideDevicesForAllScenarios(devices: _devices)
+      ..addScenario(widget: TestApp());
+    await tester.pumpDeviceBuilder(builder);
+    await screenMatchesGolden(tester, name);
   });
+
 }
